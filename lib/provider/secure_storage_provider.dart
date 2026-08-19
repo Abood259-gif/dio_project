a58c953769a6c3d3c84b558779af6c3780f50bae
Abdulrahman Mohammed <aboodsamt84@gmail.com>
2026-08-19 12:40:05 +0300

    adding platform options

--- lib/provider/secure_storage_provider.dart
+++ lib/provider/secure_storage_provider.dart
@@ -1,10 +1,16 @@
 
 
-import 'package:dio_project/service/storge/secure_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 final secureStorageProvider = Provider<FlutterSecureStorage>((ref) {
-  final secureStorage = const FlutterSecureStorage();
+  final secureStorage = const FlutterSecureStorage(
+    aOptions: AndroidOptions(
+      resetOnError: true
+    ),
+    iOptions: IOSOptions(
+      accessibility: KeychainAccessibility.first_unlock,
+    ),
+  );
   return secureStorage;
 });
